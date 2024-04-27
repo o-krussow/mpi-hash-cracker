6eff2e0c09c7a62dd9b012915c5aa01376f4108f
Owen Krussow <okrus-dev@tuta.io>
2024-04-27 10:49:13 -0500

    Added more info to readme and cleaned up c

--- README.md
+++ README.md
@@ -1,5 +1,53 @@
 # mpi_hash_cracker
 
-Efficiency-wise this is nothing to write home about, but it was fun to make.
+This is nowhere near as fast as something like hashcat, more a learning exercise than anything else.
 
-The main idea to this is converting from decimal numbers to base k numbers (where k is the length of the character set that we're using) so that we can arbitrarily "skip forward" different combinations of numbers. This makes it very easy to parallelize hashing combinations of characters across an arbitrary number of MPI slots/cores. Obviously this does not have the optimizations that something like hashcat does, but you can throw as many nodes at this as you can get your hands on. Networking won't be a bottleneck because each instance doesn't even need to communicate with other instances.
+The main idea to this is converting from decimal numbers to base k numbers (where k is the length of the character set that we're using) so that we can arbitrarily "skip forward" different combinations of numbers. This makes it very easy to parallelize hashing combinations of characters across an arbitrary number of MPI slots/cores. This makes it very easy to use without any consideration of node symmetry or performance. Network latency/bandwidth doesn't become a bottleneck because the instances do not communicate with each other, so in theory this should scale up very linearly.
+
+## Build
+Make sure that your mpi implementation is loaded:
+```
+$ module load openmpi
+```
+Now, make
+```
+$ make
+```
+
+## An example of running across 560 different cores
+This doesn't use very much memory so you (could) adjust this.
+```
+$ salloc -n 560 --mem=0
+```
+Running mpi_hash_cracker within your allocation:
+```
+$ mpirun mpi_hash_cracker 5d41402abc4b2a76b9719d911017c592
+
+...
+
+Current guess:  
+Current guess: z5 .
+Current guess: 1Pa4
+Current guess: ahAbV
+Current guess: aIlcN
+Current guess: a9;dF
+Current guess: bp2ex
+Current guess: bQMfp
+Current guess: b'xgh
+Current guess: cyih 
+Current guess: cY<h.
+Current guess: dfYi4
+Current guess: dGJjV
+Current guess: d8ukN
+Current guess: eoflF
+Current guess: eO,mx
+Current guess: e?Vnp
+Current guess: fwGoh
+Current guess: fXrp 
+Current guess: gecp.
+Current guess: gE8q4
+Current guess: g6SrV
+n283 found string is hello
+```
+
+Across 560 (mixed) E5 2660 v1/v2 cores, I found that I was able to do ~180MH/sec (md5). Not too terrible.
--- mpi_hash_cracker.c
+++ mpi_hash_cracker.c
@@ -8,45 +8,47 @@ char* charset = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
 
 char *strrev(char *str)
 {
-      char *p1, *p2;
+  char *p1, *p2;
 
-      if (! str || ! *str)
-            return str;
-      for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
-      {
-            *p1 ^= *p2;
-            *p2 ^= *p1;
-            *p1 ^= *p2;
-      }
-      return str;
+  if (! str || ! *str)
+        return str;
+  for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
+  {
+        *p1 ^= *p2;
+        *p2 ^= *p1;
+        *p1 ^= *p2;
+  }
+  return str;
 }
 
 void decimal_to_base_k(long long int decimal_num, char* base_k_num) {
-	int new_base = strlen(charset); //For readability sake 
+  int new_base = strlen(charset); //For readability sake 
 
-	int index = 0;
-	while (decimal_num / new_base != 0) {
-		base_k_num[index] = charset[decimal_num % new_base];
-		decimal_num = decimal_num / new_base;
-		index++;
-	}
-	//Get character at index (decimal_num % new_base)
-	base_k_num[index] = charset[decimal_num % new_base];
+  int index = 0;
+  while (decimal_num / new_base != 0) {
+    base_k_num[index] = charset[decimal_num % new_base];
+    decimal_num = decimal_num / new_base;
+    index++;
+  }
+  //Get character at index (decimal_num % new_base)
+  base_k_num[index] = charset[decimal_num % new_base];
 
-	strrev(base_k_num);
+  strrev(base_k_num);
 
-	base_k_num[index+1] = '\0';
+  base_k_num[index+1] = '\0';
 }
 
 void bytes2md5(char *data, int len, char *md5buf) {
-  // Based on https://www.openssl.org/docs/manmaster/man3/EVP_DigestUpdate.html
   EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
   const EVP_MD *md = EVP_md5();
+
   unsigned char md_value[EVP_MAX_MD_SIZE];
   unsigned int md_len, i;
+
   EVP_DigestInit_ex(mdctx, md, NULL);
   EVP_DigestUpdate(mdctx, data, len);
   EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+
   EVP_MD_CTX_free(mdctx);
   for (i = 0; i < md_len; i++) {
     snprintf(&(md5buf[i * 2]), 16 * 2, "%02x", md_value[i]);
@@ -54,53 +56,53 @@ void bytes2md5(char *data, int len, char *md5buf) {
 }
 
 void start_hashing(char* processor_name, int world_rank, int world_size, char* testhash) {
-	int MAX_PASS_LENGTH = 10;
-	long long int UPPER_LIMIT = pow(strlen(charset), MAX_PASS_LENGTH);
+  int MAX_PASS_LENGTH = 10;
+  long long int UPPER_LIMIT = pow(strlen(charset), MAX_PASS_LENGTH);
 
-	char base_k_num[200];
-	long long int decimal_counter = world_rank;
+  char base_k_num[100];
+  long long int decimal_counter = world_rank;
   char md5hash[33];
 
-	while (decimal_counter < UPPER_LIMIT) {
-		decimal_to_base_k(decimal_counter, base_k_num);
+  while (decimal_counter < UPPER_LIMIT) {
+    decimal_to_base_k(decimal_counter, base_k_num);
 
 
     bytes2md5(base_k_num, strlen(base_k_num), md5hash);
 
-		if (decimal_counter % 10000000 == 0) {
-			printf("Current guess: %s\n", base_k_num);
-		} 
+    if (decimal_counter % 10000000 == 0) {
+      printf("Current guess: %s\n", base_k_num);
+    } 
 
-		if (strcmp(md5hash, testhash) == 0) {
-			printf("%s found password is %s\n", processor_name, base_k_num);
-			break;
-		}
-		decimal_counter += world_size;
-	}	
+    if (strcmp(md5hash, testhash) == 0) {
+      printf("%s found string is %s\n", processor_name, base_k_num);
+      break;
+    }
+    decimal_counter += world_size;
+  }	
 }
-
 
 int main(int argc, char* argv[]) {
 
-        MPI_Init(NULL, NULL);      // initialize MPI environment
-        int world_size; // number of processes
-        MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Init(NULL, NULL);      // initialize MPI environment
+  int world_size; // number of processes
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-        int world_rank; // the rank of the process
-        MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_rank; // the rank of the process
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-        char processor_name[MPI_MAX_PROCESSOR_NAME]; // gets the name of the processor
-        int name_len;
-        MPI_Get_processor_name(processor_name, &name_len);
+  char processor_name[MPI_MAX_PROCESSOR_NAME]; // gets the name of the processor
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
 
-        if (argc == 2) {
-          start_hashing(processor_name, world_rank, world_size, argv[1]);
-        }
+  if (argc == 2) {
+    start_hashing(processor_name, world_rank, world_size, argv[1]);
+  }
 
-        MPI_Abort(MPI_COMM_WORLD, 0);
+  MPI_Abort(MPI_COMM_WORLD, 0);
 
-        MPI_Finalize();
+  MPI_Finalize();
 
-        return 0;
+  return 0;
 
 }
+
